cbfe50ff9088f070a260a9886cc1b04195276e24
Indanarishi <indana6969@gmail.com>
2020-09-08 05:31:36 +0700

    done

---
+++ build/precache-manifest.a0d81b7eaf46771e0b6d2b667cc89334.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3817efc0f837e253183e25f1ea971769",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48fd60f1091b6d83a595",
+    "url": "/static/js/2.8656695d.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/static/js/2.8656695d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1668f73ed9e17ae00d2",
+    "url": "/static/js/main.0f6aaa3f.chunk.js"
+  },
+  {
+    "revision": "fd5ca2d83b2880fa4599",
+    "url": "/static/js/runtime-main.28dc6147.js"
+  }
+]);
